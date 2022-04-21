0828babac975528fe3e1d8a5b7853c2fc30c57b6
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-04-21 09:32:28 +0100

    added page routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage'
+import About from './pages/About'
+import Products from './pages/Products'
+import Contact from './pages/Contact'
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          
+          <Route path="/" element={ <Homepage /> } />
+          <Route path="/about" element={ <About /> } />
+          <Route path="/products" element={ <Products /> }/>
+          <Route path="/contact" element={ <Contact /> } />
         </Routes>
     </div>
     </Router>

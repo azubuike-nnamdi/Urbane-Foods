70f907d6784b0ccaaa33600a07ec836f2f6ee4e8
Solo7903 <68706446+Fave7903@users.noreply.github.com>
2022-04-22 06:59:29 +0000

    Added products page

---
+++ src/components/Product.jsx
@@ -0,0 +1,115 @@
+
+
+function Product() {
+  return (
+    <div>
+      <div class="tab-content py-5 mx-5 mb-5">
+                        <div id="tab-1" class="tab-pane fade show p-0 active">
+                            <div class="row g-4">
+                                <div class="col-lg-6">
+                                    <div class="d-flex align-items-center">
+                                        <img class="flex-shrink-0 img-fluid rounded" src="https://technext.github.io/restoran/img/menu-1.jpg" alt="" style={{width: 80}}></img>
+                                        <div class="w-100 d-flex flex-column text-start ps-4">
+                                            <h5 class="d-flex justify-content-between border-bottom pb-2 fw-bold">
+                                                <span>Fruit Combo</span>
+                                                <span class="text-warning">NGN 1,500</span>
+                                            </h5>
+                                            <small class="fst-italic">The perfect fruit combination, just for you</small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="d-flex align-items-center">
+                                        <img class="flex-shrink-0 img-fluid rounded" src="https://technext.github.io/restoran/img/menu-2.jpg" alt="" style={{width: 80}}></img>
+                                        <div class="w-100 d-flex flex-column text-start ps-4">
+                                            <h5 class="d-flex justify-content-between border-bottom pb-2 fw-bold">
+                                                <span>Lobster Sauce</span>
+                                                <span class="text-warning">NGN 2,000</span>
+                                            </h5>
+                                            <small class="fst-italic">An amazing sauce made of one of the best crustaceans - The Lobster</small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="d-flex align-items-center">
+                                        <img class="flex-shrink-0 img-fluid rounded" src="https://technext.github.io/restoran/img/menu-3.jpg" alt="" style={{width: 80}}></img>
+                                        <div class="w-100 d-flex flex-column text-start ps-4">
+                                            <h5 class="d-flex justify-content-between border-bottom pb-2 fw-bold">
+                                                <span>Vegetable Pizza</span>
+                                                <span class="text-warning">NGN 1,200</span>
+                                            </h5>
+                                            <small class="fst-italic">Six wonderful pieces of sliced pizza, with extra vegetables</small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="d-flex align-items-center">
+                                        <img class="flex-shrink-0 img-fluid rounded" src="https://technext.github.io/restoran/img/menu-4.jpg" alt="" style={{width: 80}}></img>
+                                        <div class="w-100 d-flex flex-column text-start ps-4">
+                                            <h5 class="d-flex justify-content-between border-bottom pb-2 fw-bold">
+                                                <span>Bread Buns</span>
+                                                <span class="text-warning">NGN 500</span>
+                                            </h5>
+                                            <small class="fst-italic">Bread buns mixed with pepper</small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="d-flex align-items-center">
+                                        <img class="flex-shrink-0 img-fluid rounded" src="https://technext.github.io/restoran/img/menu-5.jpg" alt="" style={{width: 80}}></img>
+                                        <div class="w-100 d-flex flex-column text-start ps-4">
+                                            <h5 class="d-flex justify-content-between border-bottom pb-2 fw-bold">
+                                                <span>Standard Lunch</span>
+                                                <span class="text-warning">NGN 5,000</span>
+                                            </h5>
+                                            <small class="fst-italic">A combination of different meals for your afternoon, Bon Apetite</small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="d-flex align-items-center">
+                                        <img class="flex-shrink-0 img-fluid rounded" src="https://technext.github.io/restoran/img/menu-6.jpg" alt="" style={{width: 80}}></img>
+                                        <div class="w-100 d-flex flex-column text-start ps-4">
+                                            <h5 class="d-flex justify-content-between border-bottom pb-2 fw-bold">
+                                                <span>Noodle Fries</span>
+                                                <span class="text-warning">NGN 1,800</span>
+                                            </h5>
+                                            <small class="fst-italic">Have you tasted noodle fries before? This one might just be the best yet</small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="d-flex align-items-center">
+                                        <img class="flex-shrink-0 img-fluid rounded" src="https://technext.github.io/restoran/img/menu-7.jpg" alt="" style={{width: 80}}></img>
+                                        <div class="w-100 d-flex flex-column text-start ps-4">
+                                            <h5 class="d-flex justify-content-between border-bottom pb-2 fw-bold">
+                                                <span>Pepperoni Pizza</span>
+                                                <span class="text-warning">NGN 2,200</span>
+                                            </h5>
+                                            <small class="fst-italic">This combination with extra cheese is for those who have a high taste for awesomeness :)</small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="d-flex align-items-center">
+                                        <img class="flex-shrink-0 img-fluid rounded" src="https://technext.github.io/restoran/img/menu-8.jpg" alt="" style={{width: 80}}></img>
+                                        <div class="w-100 d-flex flex-column text-start ps-4">
+                                            <h5 class="d-flex justify-content-between border-bottom pb-2 fw-bold">
+                                                <span>Chicken Burger</span>
+                                                <span class="text-warning">NGN 4,220</span>
+                                            </h5>
+                                            <small class="fst-italic">Delivering the very best of fried chicken</small>
+                                        </div>
+                                    </div>
+                                  </div>
+                              </div>
+                          </div>
+        </div>
+                            
+                    </div>       
+        
+
+  );
+}
+
+export default Product
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,18 +1,65 @@
 import React from 'react'
+import '../App.css'
+import Product from '../components/Product'
 
 function Products() {
   return (
     <div>
-      <div className="container-xxl py-5">
-        <div className="container py-5">
-          <div className="row">
-            <h1>
-              welcome to the product page
-            </h1>
-          </div>
-        </div>
+
+            <div class="container-xxl py-5 bg-dark hero-header mb-5">
+                <div class="container text-center my-5 pt-5 pb-4">
+                    <h1 class="display-3 text-white mb-3 animated slideInDown">Urbane Foods Amazing Products</h1>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb justify-content-center text-uppercase">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="#">About</a></li>
+                            <li class="breadcrumb-item text-white active" aria-current="page">Products</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+      
+      
+      <div className='container py-5 mb-0'>
+            <div class="text-center wow fadeInUp mt-5" data-wow-delay="0.1s">
+                    <h4 class="ff-secondary text-center text-warning fw-semi-bold">Delicious meals from Urbane Foods</h4>
+                    <h1 class="mb-5">Most Popular Delicacies</h1>
+                </div>
+
+          <div class="tab-class text-center wow fadeInUp mt-5" data-wow-delay="0.1s">
+                    <ul class="nav nav-pills d-inline-flex justify-content-center border-bottom mb-5">
+                        <li class="nav-item">
+                            <a class="d-flex align-items-center text-start mx-3 ms-0 pb-3 active" data-bs-toggle="pill" href="#tab-1">
+                                <i class="fa fa-coffee fa-2x text-warning"></i>
+                                <div class="ps-3">
+                                    <small class="text-dark">Popular</small>
+                                    <h6 class="mt-n1 mb-0 text-dark fw-bold">Breakfast</h6>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="d-flex align-items-center text-start mx-3 pb-3" data-bs-toggle="pill" href="#tab-2">
+                                <i class="fa fa-hamburger fa-2x text-warning"></i>
+                                <div class="ps-3">
+                                    <small class="text-dark">Special</small>
+                                    <h6 class="mt-n1 mb-0 text-dark fw-bold">Launch</h6>
+                                </div>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="d-flex align-items-center text-start mx-3 me-0 pb-3" data-bs-toggle="pill" href="#tab-3">
+                                <i class="fa fa-utensils fa-2x text-warning"></i>
+                                <div class="ps-3">
+                                    <small class="text-body">Lovely</small>
+                                    <h6 class="mt-n1 mb-0 fw-bold text-dark">Dinner</h6>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+            </div>
       </div>
-    </div>
+      <Product />
+          </div>
   )
 }
 
